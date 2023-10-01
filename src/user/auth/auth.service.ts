9d68d08ddcf8473e7b164643a05ee36dbd0d9bed
Alex Ejimkaraonye <whitebeard086@gmail.com>
2023-10-01 19:45:53 +0100

    Generate and send back a JWT when user is created

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 import { UserType } from '@prisma/client';
 
 interface SignupParams {
@@ -35,6 +36,17 @@ export class AuthService {
             },
         });
 
-        return user;
+        const token = await jwt.sign(
+            {
+                name,
+                id: user.id,
+            },
+            process.env.JSON_TOKEN_KEY,
+            {
+                expiresIn: 50000000,
+            },
+        );
+
+        return { token };
     }
 }
