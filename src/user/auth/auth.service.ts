122773d4bb461ace15d9b56eea3f5913c743ef46
Alex Ejimkaraonye <whitebeard086@gmail.com>
2023-10-01 08:05:46 +0100

    Validate that email is not in use

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
+interface SignupParams {
+    email: string;
+    password: string;
+    name: string;
+    phone: string;
+}
 @Injectable()
 export class AuthService {
-  signup() {}
+    constructor(private readonly prismaService: PrismaService){
+        
+    }
+    async signup({ email }: SignupParams) {
+        const userExists = await this.prismaService.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        if(userExists){
+            throw new ConflictException()
+        }
+    }
 }
