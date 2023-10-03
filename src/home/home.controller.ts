2e58ef1b4b5400fdd79162fc8180da9272cb3c81
Alex Ejimkaraonye <whitebeard086@gmail.com>
2023-10-03 06:26:20 +0100

    Homes - Create endpoints

---
+++ src/home/home.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+
+@Controller('home')
+export class HomeController {
+    @Get()
+    getHomes(){
+        return []
+    }
+
+    @Get(':id')
+    getHome(){
+        return {}
+    }
+
+    @Post()
+    createHome(){
+        return {}
+    }
+
+    @Put(':id')
+    updateHome(){
+        return {}
+    }
+
+    @Delete(':id')
+    deleteHome(){
+        return true
+    }
+
+
+}
